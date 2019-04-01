4dc6391c573673b3f6c6eba8fea0843c50713ed6
Michael <somov.michail.yur@gmail.com>
2019-04-01 11:20:20 +0300

    Fix: Swagger fix

--- features/step-definitions/steps/mange_service_steps.ts
+++ features/step-definitions/steps/mange_service_steps.ts
@@ -1,10 +1,9 @@
 import { assert, expect } from 'chai';
 const { Given, When, Then } = require('cucumber');
 import httpClient from '../helpers/httpClient';
-import { getConnection } from 'typeorm';
+import { ServiceContext } from '../../../service-with-state-strategy/ServiceContext';
 
 When('I am on service endpoint I create a new service with default status', async function () {
-    await getConnection().synchronize(true);
     const response = await httpClient.post('/service', {});
     this.status = response.body;
     assert.strictEqual(response.statusCode, 201);
@@ -12,4 +11,19 @@ When('I am on service endpoint I create a new service with default status', asyn
 
 Then('Status of created service should I see', function () {
     assert.strictEqual(this.status, 'new');
+});
+
+Given('I have a service with state {string}', function (state) {
+    this.state = state;
+});
+
+When('I change service state by sending new {string} to endpoint', function (action) {
+    this.context = new ServiceContext(this.state, action);
+});
+
+Then('New state status of updated service should be {string}', function (expectedState) {
+    if ( expectedState == 'exception' )
+        assert.throws(() => { this.context.run(); } , Error);
+    else
+        assert.strictEqual(this.context.run(), expectedState);
 });
